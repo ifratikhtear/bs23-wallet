df729a0c974b6b456faa4417a0151e49b6a9f411
ifratmitul <ifrat.ikhtear@gmail.com>
2023-01-06 02:06:39 +0600

    different layout routing

---
+++ src/app/main/main.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { MainRoutingModule } from './main-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    MainRoutingModule
+  ]
+})
+export class MainModule { }
