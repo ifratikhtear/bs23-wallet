eba9dfa4bf514cd31792b0a070ea942c40c3cda0
Ifrat Ikhtear Uddin <ifrat.ikhtear@brainstation-23.com>
2023-01-08 19:36:27 +0600

    naming changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'finance-dream'`, () => {
+  it(`should have as title 'bs-wallet-admin'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('finance-dream');
+    expect(app.title).toEqual('bs-wallet-admin');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('finance-dream app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('bs-wallet-admin app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'finance-dream';
+  title = 'bs-wallet-admin';
 }
