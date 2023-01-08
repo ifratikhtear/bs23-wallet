ec23410ae03eb75bfab5215655e0dee7ef44af17
Ifrat Ikhtear Uddin <ifrat.ikhtear@brainstation-23.com>
2023-01-08 19:29:22 +0600

    error handling

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NotFoundComponent,
+    ServerErrorComponent
+  ],
   imports: [
     CommonModule
   ]
